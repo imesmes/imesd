1839c459bbfbebb10b0b21a0330f465ee6e4f355
david <david@imesmes.com>
2011-09-23 13:13:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imesd::Application.config.secret_token = 'eca61f9f93c8108fd711f49a8060501fceb0fe0ae4a75defabb9a65c517161b636ac30e261551ad85a43656ce44b7668dfa768225fa5ddb1676f16fa2fd4256e'
