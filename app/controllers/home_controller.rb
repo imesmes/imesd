e991c9446fb1364c3d7af3291774422f4bdce779
david <david@imesmes.com>
2011-09-29 16:40:19 +0200

    Mustache rules

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @dogs = Dog.all
   end
 
 end
--- app/views/home/index.rb
+++ app/views/home/index.rb
@@ -2,4 +2,13 @@ class Home::Index < Mustache::Rails
   def secret?
     false
   end
+
+  def dogs
+    dogs.collect do |dog|
+      {
+        :name => dog.name,
+        :cuteness => dog.cuteness
+      }
+    end
+  end
 end
